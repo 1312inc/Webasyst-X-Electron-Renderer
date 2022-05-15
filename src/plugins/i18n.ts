e10c0b98fa93fb022c661ca1b95e93390f009c55
vofka <vofka@webasyst.com>
2022-05-15 12:25:56 +0300

    en & ru loc tweaks

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -4,50 +4,50 @@ const messages = {
   en: {
     appName: 'Cash Flow',
     signInButton: 'Sign in with Webasyst ID',
-    signInMessage: 'Know how much money you\'ll have on your business account tomorrow, in a week, in a month, and a year.<br /><br />Sign in with Webasyst ID to quickly connect to all your Webasyst accounts.',
-    logout: 'Logout',
+    signInMessage: 'Manage your business cash flow wisely. Know how much money you’ll have on your business account tomorrow, in a week, in a month, and a year. Avoid cash gaps. Forecast the financial future of your business.',
+    logout: 'Log out',
     cloud: {
       button: 'Create Webasyst Account',
-      desc: 'This app automatically shows all Webasyst account with the Cash Flow app installed. Don\'t see anything here? Head on to the Cash Flow app site and install the app. Then get back here!'
+      desc: 'A new Webasyst Cloud account will be automatically created with Cash, Team, Installer and other awesome apps pre-installed. This will be an account for your entire team.'
     },
     installCashApp: {
       title: 'Install Cash Flow on',
       button: 'Install Cash Flow',
-      desc: '@:appName app is not installed on {name} at the moment. Tap the Install button above to install the app to your Webasyst Account via the Webasyst Installer. A free license will be issued for first 30 days.',
-      trial: 'First 30 days free!'
+      desc: '@:appName app is not installed on {name} at the moment. Tap the Install button above to install the app to your Webasyst account via the Webasyst Installer.',
+      trial: '30 days free!'
     },
     loading: 'Loading...',
     errors: {
       install: {
-        404: 'Продукт не существует',
-        403: 'Токен не содержит scope installer',
-        402: 'Установка продукта требует лицензии',
-        409: 'Установка не соответствует требованиям продукта'
+        404: 'The app does not exist',
+        403: 'No access to the Installer app',
+        402: 'The app license is required',
+        409: 'The Webasyst account server does not meet the app system requirements'
       }
     }
   },
   ru: {
-    appName: 'Cash Flow',
-    signInButton: 'Sign in with Webasyst ID',
-    signInMessage: 'Know how much money you\'ll have on your business account tomorrow, in a week, in a month, and a year.<br /><br />Sign in with Webasyst ID to quickly connect to all your Webasyst accounts.',
-    logout: 'Logout',
+    appName: 'Деньги',
+    signInButton: 'Войти с Webasyst ID',
+    signInMessage: 'Знайте с точностью до рубля, сколько денег будет на счете через месяц, через полгода, через год. Прогнозируйте финансовое будущее бизнеса. Избегайте кассовых разрывов.',
+    logout: 'Выйти',
     cloud: {
-      button: 'Create Webasyst Account',
-      desc: 'This app automatically shows all Webasyst account with the Cash Flow app installed. Don\'t see anything here? Head on to the Cash Flow app site and install the app. Then get back here!'
+      button: 'Создать аккаунт Webasyst',
+      desc: 'В облаке Webasyst будет развернут новый аккаунт с предустановленными приложениями «Деньги», «Команда», «Инсталлер». Вы сможете приглашать своих коллег в этот аккаунт для совместной работы.'
     },
     installCashApp: {
-      title: 'Install Cash Flow on',
-      button: 'Install Cash Flow',
-      desc: '@:appName app is not installed on {name} at the moment. Tap the Install button above to install the app to your Webasyst Account via the Webasyst Installer. A free license will be issued for first 30 days.',
-      trial: 'First 30 days free!'
+      title: 'Установить «Деньги» на',
+      button: 'Установить «Деньги»',
+      desc: 'Приложение @:appName еще не установлено в аккаунте Webasyst {name}. Нажмите «Установить», и уже через пару мгновений приложение будет установлено в вашем аккаунте Webasyst с помощью Инсталлера.',
+      trial: '30 дней бесплатно!'
     },
-    loading: 'Loading...',
+    loading: 'Загрузка...',
     errors: {
       install: {
-        404: 'Продукт не существует',
-        403: 'Токен не содержит scope installer',
-        402: 'Установка продукта требует лицензии',
-        409: 'Установка не соответствует требованиям продукта'
+        404: 'Приложение не существует',
+        403: 'Нет доступа к «Инсталлеру»',
+        402: 'Установка приложения требует лицензии',
+        409: 'Сервер аккаунта Webasyst не соответствует системным требованиям приложения'
       }
     }
   }
